3fa20aee605bdff7c0916a616aad17551bccacdf
allie-walker <allison.s.walker@gmail.com>
2020-06-06 18:17:02 -0400

    added python notebook code

---
+++ SSN_tools.py
@@ -0,0 +1,287 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  6 11:14:09 2019
+
+@author: allis
+"""
+
+from Bio import SeqIO
+from Bio import Seq
+from Bio import AlignIO
+from Bio.Align.Applications import ClustalwCommandline
+from Bio import Phylo
+from Bio.Align.Applications import MuscleCommandline
+from Bio import pairwise2
+from xml.etree import ElementTree as ET
+import numpy as np
+import math
+import subprocess
+
+SSN_filenames = ["20799_thiolase_N_term_25_full_ssn.xgmml",
+                 "21330_abc_35_full_ssn.xgmml",
+                 "20870_acyl_transferase_pfam_100_full_ssn.xgmml",
+                 "20923_aaa_30_full_ssn.xgmml",
+                 "20928_abc2_30_full_ssn.xgmml",
+                 "20929_acyl-coa_dehydrogenase_c_30_full_ssn.xgmml",
+                 "20930_acyl-coa_dehydrogenase_n_20_full_ssn.xgmml",
+                 "20931_alcohol_dehydrogenase_groes_20_full_ssn.xgmml",
+                 "20932__alpha_beta_hydrolase_family_40_full_ssn.xgmml",
+                 "20935_aminotransferase_40_full_ssn.xgmml",
+                 "20944__beta-ketoacyl_synthase__c_55_full_ssn.xgmml",
+                 "20941_beta-ketoacyl_synthase__n_100_full_ssn.xgmml",
+                 "20942_cytochrome_p450_40_full_ssn.xgmml",
+                 "20943_degt_dnrj_eryc1_strs_aminotransferase_60_full_ssn.xgmml",
+                 "20946_enoyl-_acyl_carrier_protein_45_full_ssn.xgmml",
+                 "20947_erythronolide_synthase_docking_9_full_ssn.xgmml",
+                 "20948_fad_binding_60_full_ssn.xgmml",
+                 "20949_glycosyl_transferase_family_2_20_full_ssn.xgmml",
+                 "20953_glycosyltransferase_family_28_25_full_ssn.xgmml",
+                 "20951_glycosyl_transferases_group_1_35_full_ssn.xgmml",
+                 "20954_glycosyltransferase_like_family_2_25_full_ssn.xgmml",
+                 "20955__glyoxalase_bleomycin_resistance_35_full_ssn.xgmml",
+                 "20957_kr_domain_55_full_ssn.xgmml",
+                 "20960_lanthionine_synthetase_c-like_30_full_ssn.xgmml",
+                 "20963_major_facilitator_superfamily_45_full_ssn.xgmml",
+                 "20977_methyltransferase_small_domain_20_full_ssn.xgmml",
+                 "20976_methyltransferase_domain_45_full_ssn.xgmml",
+                 "20979_nad_dependent_epimerase_45_full_ssn.xgmml",
+                 "20982_ndp-hexose_2_3-dehydratase_70_full_ssn.xgmml",
+                 "20986_o-methyltransferase_40_full_ssn.xgmml",
+                 "20987_oxidoreductase_family__c_25_full_ssn.xgmml",
+                 "20988_oxidoreductase_family_30_full_ssn.xgmml",
+                 "20989__phosphopantetheine_attachment_25_full_ssn.xgmml",
+                 "20991_polyketide_cyclase_50_full_ssn.xgmml",
+                 "20992_polyketide_synthase_dehydratase_65_full_ssn.xgmml",
+                 "20993_protein_of_unknown_function_20_full_ssn.xgmml",
+                 "20998_short_chain_dehydrogenase_50_full_ssn.xgmml",
+                 "21000_snoal-like_30_full_ssn.xgmml",
+                 "21001_spab_c-terminal_25_full_ssn.xgmml",
+                 "21003_sugar__and_other__transporter_20_full_ssn.xgmml",
+                 "21005_transcriptional_regulatory_18_full_ssn.xgmml",
+                 "21004_thioesterase_domain_40_full_ssn.xgmml",
+                 "21006_ubie_coq5_methyltransferase_25_full_ssn.xgmml",
+                 "21008_udp-glucoronosyl_25_full_ssn.xgmml",
+                 "21009_ycao-like_40_full_ssn.xgmml",
+                 "21010_zinc-binding_dehydrogenase_40_full_ssn.xgmml",
+                 "23780_pyridine_nucleotide_oxidoreductase_full_ssn.xgmml"]
+
+SSN_map_filenames = ["20800_20799_thiolase_N_term_25_full_ssn_coloredssn_mapping_table",
+                     "21332_21330_abc_35_full_ssn_coloredssn_mapping_table",
+                     "20878_20870_acyl_transferase_pfam_100_full_ssn_coloredssn_mapping_table",
+                     "21013_20923_aaa_30_full_ssn_coloredssn_mapping_table",
+                     "21015_20928_abc2_30_full_ssn_coloredssn_mapping_table",
+                     "21016_20929_acyl-coa_dehydrogenase_c_30_full_ssn_coloredssn_mapping_table",
+                     "21017_20930_acyl-coa_dehydrogenase_n_20_full_ssn_coloredssn_mapping_table",
+                     "21018_20931_alcohol_dehydrogenase_groes_20_full_ssn_coloredssn_mapping_table",
+                     "21019_20932__alpha_beta_hydrolase_family_40_full_ssn_coloredssn_mapping_table",
+                     "21021_20935_aminotransferase_40_full_ssn_coloredssn_mapping_table",
+                     "21028_20944__beta-ketoacyl_synthase__c_55_full_ssn_coloredssn_mapping_table",
+                     "21025_20941_beta-ketoacyl_synthase__n_100_full_ssn_coloredssn_mapping_table",
+                     "21026_20942_cytochrome_p450_40_full_ssn_coloredssn_mapping_table",
+                     "21027_20943_degt_dnrj_eryc1_strs_aminotransferase_60_full_ssn_coloredssn_mapping_table",
+                     "21029_20946_enoyl-_acyl_carrier_protein_45_full_ssn_coloredssn_mapping_table",
+                     "21030_20947_erythronolide_synthase_docking_9_full_ssn_coloredssn_mapping_table",
+                     "21031_20948_fad_binding_60_full_ssn_coloredssn_mapping_table",
+                     "21032_20949_glycosyl_transferase_family_2_20_full_ssn_coloredssn_mapping_table",
+                     "21035_20953_glycosyltransferase_family_28_25_full_ssn_coloredssn_mapping_table",
+                     "21033_20951_glycosyl_transferases_group_1_35_full_ssn_coloredssn_mapping_table",
+                     "21036_20954_glycosyltransferase_like_family_2_25_full_ssn_coloredssn_mapping_table",
+                     "21037_20955__glyoxalase_bleomycin_resistance_35_full_ssn_coloredssn_mapping_table",
+                     "21039_20957_kr_domain_55_full_ssn_coloredssn_mapping_table",
+                     "21040_20960_lanthionine_synthetase_c-like_30_full_ssn_coloredssn_mapping_table",
+                     "21043_20963_major_facilitator_superfamily_45_full_ssn_coloredssn_mapping_table",
+                     "21045_20977_methyltransferase_small_domain_20_full_ssn_coloredssn_mapping_table",
+                     "21044_20976_methyltransferase_domain_45_full_ssn_coloredssn_mapping_table",
+                     "21047_20979_nad_dependent_epimerase_45_full_ssn_coloredssn_mapping_table",
+                     "21049_20982_ndp-hexose_2_3-dehydratase_70_full_ssn_coloredssn_mapping_table",
+                     "21052_20986_o-methyltransferase_40_full_ssn_coloredssn_mapping_table",
+                     "21053_20987_oxidoreductase_family__c_25_full_ssn_coloredssn_mapping_table",
+                     "21054_20988_oxidoreductase_family_30_full_ssn_coloredssn_mapping_table",
+                     "21055_20989__phosphopantetheine_attachment_25_full_ssn_coloredssn_mapping_table",
+                     "21056_20991_polyketide_cyclase_50_full_ssn_coloredssn_mapping_table",
+                     "21057_20992_polyketide_synthase_dehydratase_65_full_ssn_coloredssn_mapping_table",
+                     "21058_20993_protein_of_unknown_function_20_full_ssn_coloredssn_mapping_table",
+                     "21063_20998_short_chain_dehydrogenase_50_full_ssn_coloredssn_mapping_table",
+                     "21064_21000_snoal-like_30_full_ssn_coloredssn_mapping_table",
+                     "21065_21001_spab_c-terminal_25_full_ssn_coloredssn_mapping_table",
+                     "21067_21003_sugar__and_other__transporter_20_full_ssn_coloredssn_mapping_table",
+                     "21069_21005_transcriptional_regulatory_18_full_ssn_coloredssn_mapping_table",
+                     "21068_21004_thioesterase_domain_40_full_ssn_coloredssn_mapping_table",
+                     "21070_21006_ubie_coq5_methyltransferase_25_full_ssn_coloredssn_mapping_table",
+                     "21071_21008_udp-glucoronosyl_25_full_ssn_coloredssn_mapping_table",
+                     "21072_21009_ycao-like_40_full_ssn_coloredssn_mapping_table",
+                     "21073_21010_zinc-binding_dehydrogenase_40_full_ssn_coloredssn_mapping_table",
+                     "23815_23780_pyridine_nucleotide_oxidoreductase_full_ssn_coloredssn_mapping_table"]
+
+ssn_thresholds = [25,35,100,30,30,30,20,20,40,40,55,100,40,60,45,9,60,
+                  20,25,35,25,35,55,30,45,20,45,45,70,40,25,30,
+                  25,50,65,20,50,30,25,20,18,40,25,25,40,40,30]
+
+def findAllPFAMSeqs(genbank_file, pfam_name):
+    cluster_domains = []
+    score_cutoff = 20
+
+    record = SeqIO.read(open(genbank_file, 'rU'),"genbank")
+    features = record.features
+    for feature in features:
+        if feature.type == "PFAM_domain":
+            score = float(feature.qualifiers["score"][0])
+            if score <score_cutoff:
+                continue
+            domain_description = feature.qualifiers["description"][0]
+            #domain_description = domain_description.lower().replace("\n", " ")
+            
+            if pfam_name == domain_description:
+                cluster_domains.append(feature.qualifiers["translation"])
+    return cluster_domains
+
+def makePFAMNameDictionary(SSN_pfam_names, SSN_feature_names):
+    prev_feature_name = ""
+    pfam_name_dictionary = {}
+    i = 0
+    for name in SSN_feature_names:
+        if name[0:name.rfind("_")] == prev_feature_name:
+            continue
+        pfam_name_dictionary[SSN_pfam_names[i]] = name[0:name.rfind("_")]
+        prev_feature_name = name[0:name.rfind("_")]
+        i+=1
+    return pfam_name_dictionary
+
+def generateSSNFeatureMatrix(clusters, SSN_pfam_names, SSN_feature_names, included_clusters, blast_exe):
+    pfam_name_dictionary =makePFAMNameDictionary(SSN_pfam_names, SSN_feature_names)
+    SSN_feature_matrix = np.zeros((len(clusters), len(SSN_feature_names)))
+    j = 0
+    for c in clusters:
+        filename = c 
+
+        i = 0
+        for pfam in SSN_pfam_names:
+            pfam_sequences = findAllPFAMSeqs(filename, pfam)
+            for sequence in pfam_sequences:
+                ssn_seq_filename = "SSN/gene_sequences/" + pfam_name_dictionary[pfam] +"_domains.fasta"
+                ssn_membership =findSSNMembership(c, sequence, ssn_seq_filename, i, included_clusters[pfam_name_dictionary[pfam]], blast_exe)
+                for cluster in ssn_membership:
+                    if pfam_name_dictionary[pfam] + "_" + cluster not in SSN_feature_names:
+                        print "no match"
+                        continue
+                    SSN_feature_matrix[j,SSN_feature_names.index(pfam_name_dictionary[pfam] + "_" + cluster)] +=1  
+            i+= 1
+        j += 1
+    return SSN_feature_matrix
+
+def getGeneName(network_fn):
+    tree = ET.parse(network_fn)
+    root = tree.getroot()
+    node_to_gene_ids = {}
+    for child in root:
+        if "node" in child.tag:
+            node_id = child.attrib['id']
+            #print node_id
+            gene_id = child[1][0].attrib['value']
+            node_to_gene_ids[node_id] = gene_id
+    return node_to_gene_ids
+
+def parseBLAST(blast_out):
+    return
+
+def findSSNMembership(cluster_name, sequence, ssn_seq_filename, ssn_index, included_clusters, blast_exe):
+    #read all the sequences from the sequence file, discard any with the same name as current cluster
+    #print cluster_name
+    #print ssn_seq_filename
+    seq_file = open(ssn_seq_filename, 'r')
+    sequences = {}
+    for line in seq_file:
+        if ">" in line:
+            gene_name = line.replace("\n","").replace(">","").replace("\r","")
+        else:
+            sequences[gene_name] = line.replace("\n","")
+    #group sequences by SSN cluster
+    sequences_in_SSN = {}
+    gene_ids_in_SSN = {}
+    SSN_filename = SSN_filenames[ssn_index]
+    node_to_gene = getGeneName("SSN/" +SSN_filename)
+    SSN_map_file =open("SSN/" +SSN_map_filenames[ssn_index] + ".txt")
+    for line in SSN_map_file:
+        #print line
+        if "UniProt" in line:
+            continue
+        node_id = line.split()[0]
+        cluster_id = line.split()[1]
+        if cluster_id not in included_clusters:
+            continue
+        gene_id = node_to_gene[node_id]
+        if cluster_name in gene_id:
+            continue
+        if cluster_id not in sequences_in_SSN:
+            sequences_in_SSN[cluster_id] = []
+            gene_ids_in_SSN[cluster_id] = []
+        sequences_in_SSN[cluster_id].append(sequences[gene_id])
+        gene_ids_in_SSN[cluster_id].append(gene_id)
+    SSN_map_file.close()
+    
+    #figure out which clusters sequences with e-value score lower than threshold
+    clusters_with_match = []
+    threshold = ssn_thresholds[ssn_index]
+    waiting = True
+    while waiting:
+        temp_fasta = open("temp_file/seq1.fasta", 'w')
+        waiting = False
+        try:
+            temp_fasta = open("temp_file/seq1.fasta", 'w')
+            waiting = False
+        except:
+            print "file error!!"
+            
+    temp_fasta.write(">"+cluster_name+"\n")
+    temp_fasta.write(sequence[0])
+    temp_fasta.close()
+    for cluster in sequences_in_SSN:
+        #print cluster
+        for seq in sequences_in_SSN[cluster]:
+            waiting = True
+            while waiting:
+                try:
+                    temp_fasta = open("temp_file/seq2.fasta", 'w')
+                    waiting = False
+                except:
+                    print "file error!!"
+                                    
+            temp_fasta.write(">"+cluster +"\n")
+            temp_fasta.write(seq)
+            temp_fasta.close()
+            gap_open = '11' 
+            gap_extend = '1'
+            comp_based = 2
+            cline = '{0} -query temp_file/seq1.fasta -subject temp_file/seq2.fasta -gapopen {1} -gapextend {2} -comp_based_stats {3} -use_sw_tback -outfmt \"6\" -max_hsps 1 -evalue {4}'.format(blast_exe,gap_open,gap_extend,comp_based,5)
+            child = subprocess.Popen(cline,stdout=subprocess.PIPE, shell=True)
+            my_out,my_err = child.communicate()
+            score = 0
+            #print sequence[0]
+            #print ""
+            #print seq
+            #print my_out
+            if len(my_out.split()) >=12:
+                score = float(my_out.split()[11])
+                score = -1*math.log10((2**(-1*score))*len(sequence[0])*len(seq))
+            #print score
+            
+            if score > threshold:
+                    #print threshold
+                    #print "score: " + str(score)
+                    clusters_with_match.append(cluster)
+                    break
+            
+            cline = '{0} -query temp_file/seq2.fasta -subject temp_file/seq1.fasta -gapopen {1} -gapextend {2} -comp_based_stats {3} -use_sw_tback -outfmt \"6\" -max_hsps 1 -evalue {4}'.format(blast_exe,gap_open,gap_extend,comp_based,5)
+            #child = subprocess.Popen(cline,shell=True,stdout=subprocess.PIPE,close_fds=True)
+            child = subprocess.Popen(cline,stdout=subprocess.PIPE,shell=True)
+            my_out,my_err = child.communicate()
+            if len(my_out.split()) >=12:
+                score = float(my_out.split()[11])
+                score = -1*math.log10((2**(-1*score))*len(sequence[0])*len(seq))
+           
+            if score > threshold:
+                    clusters_with_match.append(cluster)
+                    break
+    #(cluster_counts, cluster_list) = readSSNFile(name)
+    #print clusters_with_match
+    return clusters_with_match
---
+++ readFeatureFiles.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 17 14:14:27 2018
+
+@author: allis
+"""
+
+import numpy as np
+from sklearn import linear_model
+import random
+
+def readFeatureMatrix(filename):
+    in_file = open(filename,'r')
+    feature_matrix = []
+    for line in in_file:
+        if "," not in line:
+            continue
+        entries = line.split(",")
+        feature_matrix.append([])
+        for i in range(0,len(entries)-1):
+            feature_matrix[len(feature_matrix)-1].append(int(entries[i]))
+    in_file.close()
+    return np.array(feature_matrix)
+
+def readFeatureMatrixFloat(filename):
+    in_file = open(filename,'r')
+    feature_matrix = []
+    for line in in_file:
+        if "," not in line:
+            continue
+        entries = line.split(",")
+        feature_matrix.append([])
+        for i in range(0,len(entries)-1):
+            feature_matrix[len(feature_matrix)-1].append(float(entries[i]))
+    in_file.close()
+    return np.array(feature_matrix)
+            
+def readFeatureList(filename):
+    in_file = open(filename,'r')
+    feature_list = []
+    for line in in_file:
+        feature_list.append(line.replace("\n",""))
+    return feature_list
+
+def readClassesMatrix(filename):
+    class_matrix = []
+    in_file = open(filename,'r')
+    for line in in_file:
+        if len(line)>0:
+            class_matrix.append(int(line))
+    in_file.close()
+    return np.array(class_matrix)     
+       
+def getNotUnknownIndices(is_unknown):
+    is_not_unknown_indices = []
+    for i in range(0, len(is_unknown)):
+        if not is_unknown[i]:
+            is_not_unknown_indices.append(i)
+    return is_not_unknown_indices
+    
+def writeROC(probs, validation_y):
+    outfile_prob= open("classifier_results/logistic_antibiotic_prob_vs_tp3.txt",'w')
+    outfile_roc= open("classifier_results/logistic_antibiotic_roc3.txt",'w')
+    outfile_raw_count= open("classifier_results/logistic_antibiotic_raw_count3.txt",'w')
+    is_class_prob = probs[:,1]
+    for i in range(0, 21):
+        cutoff_prob = i*.05
+        classifications = (is_class_prob>cutoff_prob).astype(int)
+        #print classifications
+        true_positives = classifications*validation_y
+        false_positives = (1-validation_y)*classifications
+        tp_rate = (true_positives.sum()*1.0)/validation_y.sum()
+        fp_rate = (false_positives.sum()*1.0)/(1-validation_y).sum()
+        outfile_prob.write(str(1-i)+","+str(tp_rate)+"\n")
+        outfile_roc.write(str(fp_rate)+","+str(tp_rate)+"\n")
+        outfile_raw_count.write(str(false_positives.sum()) +"," + str(true_positives.sum()) + "\n")
+    outfile_prob.close()
+    outfile_roc.close()
+    outfile_raw_count.close()
+    
+def readClusterList(filename):
+    infile = open(filename,'r')
+    cluster_list = []
+    for line in infile:
+        cluster_list.append(line.replace("\n",""))
+    infile.close()
+    return cluster_list
+
+def getFeatureName(i, pfam_list, card_list, smCOG_list):
+    merged_list = pfam_list+card_list+smCOG_list
+    return merged_list[i]
+    if i < len(pfam_list):
+         print pfam_list[-3]
+         return pfam_list[i]
+    elif i < len(pfam_list)+len(card_list):
+        return card_list[i-len(pfam_list)]
+    else:
+        return smCOG_list[i-len(pfam_list)-len(card_list)]
+                              
+#def normalizeFeatures(feature_matrix, method):
+    #if method == "MIN
+    #return
